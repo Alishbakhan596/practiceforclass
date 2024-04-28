a19994b0348339e2e2c2b3a6bea1e166c2806f06
fatma278 <fatimahaasss@gmail.com>
2024-04-28 13:42:03 +0500

    practice.class

---
+++ main.dart
@@ -0,0 +1,28 @@
+void main () {
+//int a = 1;
+//do {
+  //print (a);
+ //a++;
+//} while (a == 10);
+
+//String username = "Alishba";
+//String password = "596";
+//nt i = 0;
+//do {
+// print (i);
+ // i++;
+//} while (i < 10);
+ 
+ printTable (5);
+ print("==========");
+ printTable (6);
+ print("==========");
+ printTable (7);
+ print("==========");
+}
+
+ printTable (int table) {
+  for (var i = 1 ; i <= 10 ; i++) {
+    print (i * table);
+  }
+ }
